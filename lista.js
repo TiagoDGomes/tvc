af72d1d3c8ca5dbd3a4ba59ff5381d540608f449
Tiago Donizetti Gomes <tiago.gomes@ifsp.edu.br>
2019-04-06 20:04:32 -0300

    v4

---
+++ lista.js
@@ -0,0 +1,76 @@
+var request = null;
+
+function createRequest() {
+    try {
+        request = new XMLHttpRequest();
+    } catch (trymicrosoft) {
+        try {
+            request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (othermicrosoft) {
+            try {
+                request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (failed) {
+                request = null;
+            }
+        }
+    }
+
+    if (request == null)
+        console.log("Error creating request object!");
+}
+
+function atualiza_lista_geral() {
+    if (request.readyState == 4) {
+        var lista = JSON.parse(request.responseText);
+        var url_string = window.location.href;
+        var url = new URL(url_string);
+        var local = url.searchParams.get("local");
+        console.log(lista);
+        conversa_lista.innerHTML = '';
+
+        var mensagem_inicial = lista[local]['mensagem_inicial'];
+        if (mensagem_inicial != undefined && mensagem_inicial != ''){
+            escrever_mensagem(mensagem_inicial);
+        }
+        
+        if (lista[local]['coisas'] != undefined && lista[local]['coisas'].length > 0){
+            for (i = 0; i < lista[local]['coisas'].length; i++) {  
+                var container = document.createElement('div');   
+                container.className = 'produto';       
+                var titulo = document.createElement('h2');
+                titulo.innerHTML = lista[local]['coisas'][i]['nome'];
+                container.appendChild(titulo);
+                var link = document.createElement('a');
+                link.href = "lista_escolhido.html?escolha=" + lista[local]['coisas'][i]['nome'];
+
+                var imagem = document.createElement('img');
+                imagem.src = lista[local]['coisas'][i]['imagem'];
+                imagem.alt = lista[local]['coisas'][i]['palavra-chave'];
+                link.appendChild(imagem);
+                container.appendChild(link);
+                escrever_mensagem_complexa(container);
+                
+            }
+        } else {
+            escrever_mensagem('???');
+            escrever_mensagem('Acho que esqueci de algo...');
+            escrever_mensagem('Ah, lembrei');            
+            escrever_mensagem('Não preparei a lista... sou muito burro');
+        }        
+
+        var mensagem_final = lista[local]['mensagem_final'];
+        if (mensagem_final != undefined && mensagem_final != ''){
+            escrever_mensagem(mensagem_final);
+        }
+        
+        pipocar_conversa(local,true);
+    }
+}
+
+function get_conversa_presentes(func_atualizar) {
+    createRequest();
+    var url = "conversa_presentes.json?__=" + Math.floor((Math.random() * 100000) + 1);
+    request.open("GET", url, true);
+    request.onreadystatechange = func_atualizar;
+    request.send(null);
+}
--- whatsapp.js
+++ whatsapp.js
@@ -1,9 +1,56 @@
 var conversa = document.getElementsByClassName('conversa');
 var conversa_i = 0;
+var lista_pipocada;
 
-setInterval(function () {
-    if (conversa_i < conversa.length) {
-        conversa[conversa_i].style.display = 'inherit';
-        conversa_i++;
+function pipocar_conversa(local_atual, force) {
+    console.log('force 1:', force)
+    if (force == undefined){
+        try {
+            lista_pipocada = localStorage.getItem(local_atual + '_ja_pipocada');
+            if (lista_pipocada=='true'||lista_pipocada==true){
+                force = false;
+            } else {
+                force = true;
+            }
+        } catch (e) {
+            force = true;
+        }    
+    } 
+    console.log('force 2:', force)
+    
+    if (force) {
+        for (i = 0; i < conversa.length; i++) {
+            conversa[i].style.display = 'none';
+        }
+
+        var conversa_pipocando = setInterval(function () {
+            if (conversa_i < conversa.length) {
+                conversa[conversa_i].style.display = 'inherit';
+                conversa_i++;
+            } else {
+                clearInterval(conversa_pipocando);
+                localStorage.setItem(local_atual + '_ja_pipocada', true);
+            }
+        }, 1000);
     }
-}, 1000);
+}
+
+function escrever_mensagem(texto){
+    var conversa_lista = document.getElementById('conversa_lista');
+    var conversa_item = document.createElement('section');
+    conversa_item.className = 'conversa';
+    conversa_item.innerHTML = texto;
+    conversa_lista.appendChild(conversa_item);
+    //$(conversa_item).delay(800*i).fadeIn(400);
+}
+
+
+function escrever_mensagem_complexa(obj){
+    var conversa_lista = document.getElementById('conversa_lista');
+    var conversa_item = document.createElement('section');
+    conversa_item.className = 'conversa';
+    conversa_item.appendChild(obj);
+    conversa_lista.appendChild(conversa_item);
+    //$(conversa_item).delay(800*i).fadeIn(400);
+}
+
